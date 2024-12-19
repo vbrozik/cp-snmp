21c152aeb7fc12500a5c1d1213ef56584ae38049
Václav Brožík <5073939+vbrozik@users.noreply.github.com>
2024-12-19 11:56:51 +0100

    add(snmp_test): Add lock override and robust cleanup

--- tools/snmp_test.bash
+++ tools/snmp_test.bash
@@ -4,30 +4,50 @@
 # This script prepares the environment for SNMP tests on Check Point Gaia.
 # Currently it supports only SNMPv3.
 
+# ------ Configuration ------
 
+# These OID will be tested with snmpwalk.
 snmpwalk_test_oids=(
     "SNMPv2-MIB::sysName"
     "IF-MIB::ifDescr"
     "NET-SNMP-EXTEND-MIB::nsExtendOutput2Table"
 )
 
+# SNMPv3 options for the testing user.
 security_level="authPriv"
 privacy_protocol="AES"
 authentication_protocol="SHA-256"
 
-random_username="snmp_test_$(tr -dc A-Za-z0-9 </dev/urandom | head -c 4)"
-random_password="$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)"
+# Generate a random username and password for the testing user.
+username="snmp_test_$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 4)"
+# The generated password must be at least 8 and at most 128 characters long.
+# Tested on Gaia R81.10.
+password="$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 50)"
 
-# Use credentials from the environment if available
-: "${username:=$random_username}"
-: "${password:=$random_password}"
+# Non-zero value enables debug output.
+_debug=0
 
-# ------------------------------
+# ------ End of Configuration ------
+
+
+# Check if the clish lock is held by another session. If so, override it.
+clish_take_lock () {
+    if clish -c 'show config-lock' | grep -q -F CLINFR0771 ; then
+        # If show config-lock returns:
+        # CLINFR0771  Config lock is owned by ... We know that other session has the lock.
+        if [ "$_debug" -gt 0 ] ; then
+            printf 'Overriding clish lock.\n'
+        fi
+        clish -c 'lock database override' | grep -v -F CLINFR0771
+        # Note: The command shows CLINFR0771  Config lock is owned by ...
+        # but it helps to override the lock.
+    fi
+}
 
 authentication_protocol_clish="$(printf %s "$authentication_protocol" | tr -d - )"
 
 mkdir -vp ~/.snmp/mibs/
-if [ ! -e ~/.snmp/mibs/chkpnt.mib ]; then
+if [ ! -e ~/.snmp/mibs/chkpnt.mib ] ; then
     ln -vs "$CPDIR/lib/snmp/chkpnt.mib" ~/.snmp/mibs/
 fi
 cat > ~/.snmp/snmp.conf << +++END+++
@@ -45,20 +65,43 @@ defPrivType             $privacy_protocol
 # clish -c "set snmp agent on"
 # clish -c "set snmp agent-version v3"
 
-clish -c \
-    "add snmp usm user $username security-level $security_level \
-    auth-pass-phrase $password privacy-pass-phrase $password \
-    privacy-protocol $privacy_protocol authentication-protocol $authentication_protocol_clish"
+clish_add_snmp_usm_user=\
+"add snmp usm user $username security-level $security_level \
+auth-pass-phrase $password privacy-pass-phrase $password \
+authentication-protocol $authentication_protocol_clish privacy-protocol $privacy_protocol"
+
+if [ "$_debug" -gt 0 ] ; then
+    printf 'Generated username: %s\n' "$username"
+    printf 'Generated password: %s\n' "$password"
+    printf 'Command length: %d\n' "${#clish_add_snmp_usm_user}"
+    # printf 'Command: %s\n' "$clish_add_snmp_usm_user"
+fi
+
+# Remove the temporary testing user before exiting.
+cleanup () {
+    clish -c "delete snmp usm user $username"
+    if [ "$_debug" -gt 0 ] ; then
+        printf 'Removed temporary testing user %s\n' "$username"
+    fi
+}
+
+clish_take_lock
+trap cleanup EXIT
+
+clish -c "$clish_add_snmp_usm_user"
 
 # Options in snmp.conf:
 # -v3 -l authPriv -a "$authentication_protocol" -x "$privacy_protocol"
 
-printf %s\\n\\n "Testing snmpwalk on localhost with temporary user $username"
+printf 'Testing snmpwalk on localhost with temporary user %s\n\n' "$username"
 
-for oid in "${snmpwalk_test_oids[@]}"; do
-    printf %s\\n "--- snmpwalk $oid ---"
+for oid in "${snmpwalk_test_oids[@]}" ; do
+    printf -- '--- snmpwalk %s ---\n' "$oid"
     snmpwalk -u "$username" -A "$password" -X "$password" localhost "$oid"
     printf \\n
 done
 
-clish -c "delete snmp usm user $username"
+# If we did not exit yet, perform the cleanup explicitly.
+
+cleanup
+trap - EXIT
